b1b79bd464f56c98f7106af2471baffb84e6e623
emiliodifabio <emiliodifabio@me.com>
2022-11-29 17:41:24 +0100

    Implemented spinner and fixed videocard reload

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -1,11 +1,36 @@
 export default class View {
   _data;
 
+  renderSpinner() {
+    const markup = `<div class="spinner"></div>`;
+    // this._videocard.innerHTML = "";
+    // this._parentEl.insertAdjacentHTML("afterbegin", markup);
+    this._videocard.innerHTML = markup;
+  }
+  renderSpinnerLocal() {
+    const markup = `<div class="spinner"></div>`;
+    this._parentEl.innerHTML = "";
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+  }
+
+  _clear() {
+    this._inputBox.value = "";
+  }
+
   render(data) {
     // if (!data || (Array.isArray(data) && data.length === 0))
     //   return this.renderError();
     this._data = data;
     const markup = this._generateMarkup();
+    this._parentEl.insertAdjacentHTML("afterbegin", markup);
+    this._clear();
+  }
+  renderLocal(data) {
+    // if (!data || (Array.isArray(data) && data.length === 0))
+    //   return this.renderError();
+    this._data = data;
+    const markup = this._generateMarkup();
+    this._parentEl.innerHTML = "";
     this._parentEl.insertAdjacentHTML("afterbegin", markup);
     this._clear();
   }
@@ -18,11 +43,13 @@ export default class View {
   showVideo() {
     this._parentEl.addEventListener("click", function (e) {
       e.preventDefault();
-
       const videoCard = e.target.closest(".videocard");
       if (!videoCard) return;
       const id = videoCard.getAttribute("data-id");
-      const cover = videoCard.firstElementChild;
+      const image = document.querySelector(".img");
+      if (image == null) return;
+
+      const cover = image.closest(".videocard--img");
 
       const iFrame = `<iframe width="320" height="180" src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
 
@@ -43,9 +70,5 @@ export default class View {
     ) {
       this._btnDeleteAll.classList.add("hidden");
     }
-  }
-
-  _clear() {
-    this._inputBox.value = "";
   }
 }
